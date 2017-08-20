a01b1bc3b354e6336470bfad05951d7db5289180
goransv <support@appernetic.io>
2017-08-20 22:02:17 +0200

    Add back missing return to top script
    Add back missing return to top script.

--- static/js/site.js
+++ static/js/site.js
@@ -3,3 +3,8 @@ $(
     $("table").addClass("table table-condensed table-bordered table-striped");
   }
 );
+  $('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
